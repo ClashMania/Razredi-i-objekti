257925fe77580610f4529b3901ee83c864f4b92c
JustGabi <113007885+ClashMania@users.noreply.github.com>
2023-12-19 11:23:33 +0100

    Add files via upload

---
+++ Razredi i objekti/Neboder.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razredi_i_objekti
+{
+    internal class Neboder
+    {
+
+        double visina, brojKatova;
+
+
+
+        public void SetVisina(int visina)
+        {
+            this.visina = visina;
+        }
+        public void SetBrojKatova(int brojKatova)
+        {
+            this.brojKatova = brojKatova;
+        }
+
+
+
+        public double GetVisina()
+        {
+            return this.visina;
+        }
+        public double GetBrojKatova()
+        {
+            return this.brojKatova;
+        }
+
+
+
+        public Neboder()
+        {
+
+        }
+
+        public Neboder(double visina, double brojKatova)
+        {
+            this.visina = visina;
+            this.brojKatova = brojKatova;
+
+        }
+
+        public override string ToString()
+        {
+            visina += 10;
+            brojKatova += 1;
+            double average = visina / brojKatova;
+            string ispis = "Visina: " + visina + ""
+                    + "\r\nBroj Katova: " + brojKatova
+                    + "\nProsjek: " + average;
+            return ispis;
+        }
+    }
+}
---
+++ Razredi i objekti/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Razredi_i_objekti
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Neboder neboder = new Neboder();
+            neboder.SetVisina(200);
+            neboder.SetBrojKatova(100);
+
+            Console.WriteLine("Tower: ");
+            Console.WriteLine(neboder.ToString());
+                        
+            Console.ReadKey();
+        }
+    }
+}
